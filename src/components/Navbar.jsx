f3df25f4d772f86152ee11ea6beeb486ca7ece8a
Thitikarn <thitikarn.ktpn@gmail.com>
2024-11-01 11:44:39 +0700

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <div className="bg-gray-800 ">
+        <div className="bg-gray-800 rounded-md">
             <nav>
-                <ul className="flex space-x-4">
+                <ul className="flex space-x-4 p-2">
                     <li className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
                         <Link to="/">Home</Link>
                     </li>
